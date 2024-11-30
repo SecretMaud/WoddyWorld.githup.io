6621a4132adcbeb475c828d0657f49dd0f5c5a79
WoddyWorld <secretmaud@gmail.com>
2024-11-30 11:31:51 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,20 +6,19 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/public'));
 
 io.on('connection', (socket) => {
     console.log('a user connected');
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
-
     socket.on('chat message', (msg) => {
         io.emit('chat message', msg);
+    });
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
     });
 });
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
